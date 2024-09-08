5972f7acef1af042d3f74de88bfbfe6e426ba71b
seraphislabs <seraphislabs@gmail.com>
2024-09-08 11:23:03 -0400

    hotfix

--- Assets/Scripts/Server/buildserver.sh
+++ Assets/Scripts/Server/buildserver.sh
@@ -1,5 +1,5 @@
 mv master.cs masterserver/Program.cs
 mv master.csproj masterserver/masterserver.csproj
-mv networkdata.cs masterserver/networkdata.cs
+mv ../Shared/networkdata.cs masterserver/networkdata.cs
 cd masterserver
 dotnet build
